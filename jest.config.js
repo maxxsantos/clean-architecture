b6d1630eb1afcddfde7ebc9c8e7004a88e57a2f1
Max Simões <max.santos@icomp.ufam.edu.br>
2020-07-28 22:41:02 -0400

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.*\\.ts$': 'ts-jest'
+  }
+}
