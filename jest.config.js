05cf41bfde7fd485ec802af4a558f4ca59588fc8
Max Simões <max.santos@icomp.ufam.edu.br>
2020-07-28 23:54:32 -0400

    fix: change configuration of jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts', // ignore any files with -protocols.ts
+    '!**/protocols/**' // ignore all files in folder named protocols
+  ],
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
   // Indicates which provider should be used to instrument code for coverage
@@ -9,6 +13,6 @@ module.exports = {
   testEnvironment: 'node',
   // A map from regular expressions to paths to transformers
   transform: {
-    '.*\\.ts$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest'
   }
 }
